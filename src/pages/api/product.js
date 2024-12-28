d00fcffa6d596b75f90edb82257e4971b1b0e9f3
Muhammad361738 <ullahehsan016@gmail.com>
2024-12-28 23:30:38 +0500

    Add new web (milestone 3) in Governor House Quater 2 journey

---
+++ src/pages/api/product.js
@@ -0,0 +1,46 @@
+export default function handler(req,res){
+    const Products = [
+        {
+            id : 1,
+            name : "Cheese Burger",
+            price : 810,
+            image : "./c1.png",
+
+        },
+        {
+            id : 2,
+            name : "French Burger",
+            price : 930,
+            image : "./c2.png",
+
+        },
+        {
+            id : 3,
+            name : "Maxican Burger",
+            price : 510,
+            image : "./c3.png",
+
+        },
+        {
+            id : 4,
+            name : "American Burger",
+            price : 1050,
+            image : "./c4.png",
+
+        },
+        {
+            id : 5,
+            name : "Chinese Burger",
+            price : 890,
+            image : "./c5.png",
+
+        },
+        {
+            id : 6,
+            name : "Arabian Burger",
+            price : 1500,
+            image : "./c6.png",
+
+        },
+    ]
+}
